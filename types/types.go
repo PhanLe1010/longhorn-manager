be58fc9c80ec8cc13029207f4239184852a015cc
Phan Le <phan.le@suse.com>
2023-09-26 11:10:54 -0700

    Make the new engine name deterministic based on the existing engine
    * If there is no engine exists yet, the name of the new engine will
    be <VolumeName>-e-0
    * if there is existing engine, increase the engine suffix by one for
    the new engine
    
    By doing this, we don't create extra new engines even if the volume
    controller run multiple sync loops with stale cache data
    
    longhorn-6682
    
    Signed-off-by: Phan Le <phan.le@suse.com>

--- types/types.go
+++ types/types.go
@@ -291,8 +291,19 @@ const (
 	replicaManagerPrefix  = instanceManagerPrefix + "r-"
 )
 
-func GenerateEngineNameForVolume(vName string) string {
-	return vName + engineSuffix + "-" + util.RandomID()
+func GenerateEngineNameForVolume(vName, currentEngineName string) string {
+	if currentEngineName == "" {
+		return vName + engineSuffix + "-" + "0"
+	}
+	parts := strings.Split(currentEngineName, "-")
+	if len(parts) < 3 {
+		return vName + engineSuffix + "-" + "1"
+	}
+	suffix, err := strconv.Atoi(parts[len(parts)-1])
+	if err != nil {
+		return vName + engineSuffix + "-" + "1"
+	}
+	return vName + engineSuffix + "-" + strconv.Itoa(suffix+1)
 }
 
 func GenerateReplicaNameForVolume(vName string) string {
